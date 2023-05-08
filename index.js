e749e3ab9ae655f2ad2f0a456d5be26f6323dfb9
Şahin Bayramoğlu <shnbayramoglu@gmail.com>
2023-05-08 10:58:07 +0300

    Update

---
+++ index.js
@@ -0,0 +1,104 @@
+const stream = require('stream')
+const Router = require('router')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const debug = require('debug')('dss')
+
+
+const router = Router()
+router.__dataStore = {}
+
+const morganDebugStream = new stream.Writable({
+  write: function (chunk, encoding, done) {
+    // strip newlines (to avoid extra empty log items in the 'tiny' morgan protocol)
+    const chunkData = chunk.toString().replace(/[\n\r]/g, '')
+
+    if (chunkData.length > 0) {
+      debug(chunkData)
+    }
+    done()
+  }
+})
+
+router.use(morgan('tiny', { stream: morganDebugStream }))
+
+router.param('id', (req, res, next, id) => {
+  req.params = {
+    id
+  }
+
+  next()
+})
+
+// parse all bodies up to 10mb regardless of mime type as a buffer
+router.use(bodyParser.raw({ limit: '10mb', type: () => true }))
+
+const bodyDebug = debug.extend('body')
+
+router.post('/data/:id', (req, res) => {
+  const deviceId = req.params.id
+
+  if (!router.__dataStore[deviceId]) {
+    router.__dataStore[deviceId] = []
+  }
+
+  // log the body, using the debug body instance
+  bodyDebug(req.body.toString())
+
+  router.__dataStore[deviceId][0] = req.body
+  var fs = require("fs")
+
+  var data = req.body
+
+  fs.writeFile("temp.txt", data, (err) => {
+    if(err) console.log(err)
+  })
+
+
+  res.statusCode = 200
+  res.end()
+})
+
+router.get('/data/:id', (req, res) => {
+  const deviceId = req.params.id
+
+
+   if (!router.__dataStore[deviceId] || router.__dataStore[deviceId].length === 0) {
+
+	 router.__dataStore[deviceId] = []
+
+
+ 	 var fs = require("fs")
+
+	 var _oldText  = fs.readFileSync("temp.txt","utf8")
+
+	 router.__dataStore[deviceId][0] = _oldText
+
+
+    const data =  _oldText
+
+    bodyDebug(_oldText)
+
+
+
+    res.statusCode = 200
+    res.end(data)
+  } else {
+
+
+
+    const data =  router.__dataStore[deviceId][0]
+
+    bodyDebug(req.body.toString())
+    res.statusCode = 200
+    res.end(data)
+  }
+})
+
+
+function returnId(id){
+   console.log(id);
+}
+
+
+module.exports = router
